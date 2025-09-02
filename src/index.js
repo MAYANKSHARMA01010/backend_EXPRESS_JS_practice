edd407c52f0121d860b2b193f36be8b76c300fcb
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-09-02 10:13:49 +0530

    Dynamic ROuting done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>My First Express Server</h1>");
+});
+
+app.get("/home", (req, res) => {
+    res.send("<h3>This is My Home Page</h3>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("This is My About Page");
+});
+
+// dynamic route
+app.get("/product/:productId", (req, res) => {
+    // every request have a params key (object)
+    res.send(`<h3>This is the Product Page for Product ID: <h2>${req.params.productId}</h2></h3>`);
+    console.log(`Trying to Access Product with ID: ${req.params.productId}`);
+});
 
 app.listen(PORT, (err) => {
     if (err) {
